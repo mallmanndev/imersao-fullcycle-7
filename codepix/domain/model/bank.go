41fc2ab55303048f9f42ddf33eaa5293c4219bee
Matheus Mallmann <matheus.mallmann@meliuz.com.br>
2022-04-05 18:49:20 -0300

    feat:Create domain layer

--- codepix/domain/model/bank.go
+++ codepix/domain/model/bank.go
@@ -1,11 +1,38 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
 
 type Bank struct {
-	ID        string
-	Code      string
-	Name      string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	Base     `valid:"required"`
+	Code     string     `json:"code" valid:"notnull"`
+	Name     string     `json:"name" valid:"notnull"`
+	Accounts []*Account `valid:"-"`
+}
+
+func (bank *Bank) isValid() error {
+	_, err := govalidator.ValidateStruct(bank)
+	return err
+}
+
+func NewBank(code string, name string) (*Bank, error) {
+	bank := Bank{
+		Code: code,
+		Name: name,
+	}
+
+	bank.ID = uuid.NewV4().String()
+	bank.CreatedAt = time.Now()
+	bank.UpdatedAt = time.Now()
+
+	err := bank.isValid()
+	if err != nil {
+		return nil, err
+	}
+
+	return &bank, nil
 }
